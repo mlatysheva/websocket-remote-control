752d8a23af7b7af417ed22d3f23b218f49a436cb
mlatysheva <mlatysheva@gmail.com>
2023-01-21 00:29:04 +0300

    refactor: change toRGB conversion method

--- src/utils/getScreenshot.ts
+++ src/utils/getScreenshot.ts
@@ -5,7 +5,8 @@ import { Region, screen } from "@nut-tree/nut-js";
 export async function getScreenshot(x: number, y: number, width: number, height: number) {
   try {
     const regionToGrab = new Region((x - width/2), (y - width/2), width, height);
-    const img = await screen.grabRegion(regionToGrab);
+    screen.highlight(regionToGrab);
+    const img = await (await screen.grabRegion(regionToGrab)).toRGB();
 
     const screenshot = new Jimp({
       data: img.data,
@@ -17,16 +18,7 @@ export async function getScreenshot(x: number, y: number, width: number, height:
       }
     });
 
-    // Bitwise transform to preserve the colors
-    
-    screenshot.scan(0, 0, screenshot.bitmap.width, screenshot.bitmap.height, (x, y, idx) => {
-      const red = screenshot.bitmap.data[idx + 0];
-      const blue = screenshot.bitmap.data[idx + 2];
-      screenshot.bitmap.data[idx + 0] = blue;
-      screenshot.bitmap.data[idx + 2] = red;
-    });
-
-    const buffer = await (await screenshot.getBufferAsync(Jimp.MIME_PNG)).toString('base64');
+    const buffer = (await screenshot.getBufferAsync(Jimp.MIME_PNG)).toString('base64');
 
     return buffer;
   } catch (error) {
